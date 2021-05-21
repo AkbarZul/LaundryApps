7f7b7dc68e8959da645fd3d2183f29880b3c9f7b
AkbarZul <akbar.zul73@gmail.com>
2021-05-21 14:36:13 +0700

    Stack Navigation

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 // import {StyleSheet} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
+import Splash from '../screens/Splash';
 
 const Stack = createStackNavigator();
 
 const Router = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator initialRouteName="Splash">
+      <Stack.Screen
+        name="Splash"
+        component={Splash}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
   );
 };
