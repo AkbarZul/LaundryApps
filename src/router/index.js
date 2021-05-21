427ae1ba65faa73403303230b7028f9f19790056
AkbarZul <akbar.zul73@gmail.com>
2021-05-21 14:50:08 +0700

    Buttom Tab navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 // import {StyleSheet} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Splash from '../screens/Splash';
+import Pesanan from '../screens/Pesanan';
+import Akun from '../screens/Akun';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Pesanan" component={Pesanan} />
+      <Tab.Screen name="Akun" component={Akun} />
+    </Tab.Navigator>
+  );
+};
 
 const Router = () => {
   return (
@@ -15,8 +29,8 @@ const Router = () => {
         options={{headerShown: false}}
       />
       <Stack.Screen
-        name="Home"
-        component={Home}
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
